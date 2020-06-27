63add5ad313a3ecf095df5d3330f94d103fbf558
gerfind <gerfind2001@gmail.com>
2020-06-27 22:23:38 +0800

    0627--完成main，更改大量错误函数，修改bug，未完成存储更改的功能

--- contacts.cpp
+++ contacts.cpp
@@ -1,35 +1,31 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-/*
- * 姓名地址中不能含有空格！！
- * 该问题难以解决，故我考虑放弃
- * （考虑到姓名地址在同一行存储，上次的方法并不能分割）
- */
+
 //结构体定义
-typedef struct node{
+typedef struct contact{
     char name[20];//姓名
     char phoneNumber[15];//号码（防止溢出、故略微加大了存储空间）
     char address[200];//住址
-    struct node *next;//next域
-}contact;
-
+    struct contact *next;//next域
+};
 contact *Read(){//建立通讯录,将文件中的通讯录信息存储进链表
     contact *head,*temp,*s;
     FILE *fp;
-    fp=fopen("通讯录.txt","r");//打开存储通讯录的文件
-    head=(contact*)malloc(sizeof(contact));//为头指针分配内存空间
+    fp=fopen("contacts.txt","r");//打开存储通讯录的文件
+    head=(struct contact*)malloc(sizeof(contact));//为头指针分配内存空间
     s=head;
     while(!feof(fp)){//文件非空
-        fscanf(fp,"%s %s %s",&temp->name,&temp->address,&temp->phoneNumber);//扫描通讯录文件中信息，存储至链表中
+        temp=(struct contact*)malloc(sizeof(contact));
+        fscanf(fp,"%s %s %s",temp->name,temp->phoneNumber,temp->address);//扫描通讯录文件中信息，存储至链表中
         s->next=temp;//尾插法建立
         s=temp;
     }
     fclose(fp);
-    s->next=NULL;//将链表最后一位的next域置空
+    s->next=0;//将链表最后一位的next域置空
     return head;//返回链表头指针
 }
-void search(contact *head) {//查找功能
+int search(contact *head) {//查找功能
     int i = 0;//查找条件控制，1为姓名查找，2为号码查找，3为住址查找
     int flag = 0;//标志查找成功和失败，成功>0,失败=0;
     char searName[20], searPhoneNum[15], searAddress[200];
@@ -40,10 +36,11 @@ void search(contact *head) {//查找功能
         case 1:
             printf("请输入姓名：");
             scanf("%s", searName);
-            while (temp->next != NULL) {//遍历链表
+            while (temp->next != 0) {//遍历链表
                 if (strcmp(temp->name, searName) == 0) {//找到符合姓名条件的联系人
                     printf("%s %s %s", temp->name, temp->phoneNumber, temp->address);
                     flag++;//查找成功，flag+1
+                    break;
                 }
                 else{
                     temp = temp->next;//否则继续向下遍历
@@ -53,7 +50,7 @@ void search(contact *head) {//查找功能
         case 2://号码查找,思路同上，不再赘述
             printf("请输入电话号码：");
             scanf("%s", searPhoneNum);
-            while (temp->next != NULL) {
+            while (temp->next != 0) {
                 if (strcmp(temp->phoneNumber, searPhoneNum) == 0) {
                     printf("%s %s %s", temp->name, temp->phoneNumber, temp->address);
                     flag++;
@@ -67,9 +64,10 @@ void search(contact *head) {//查找功能
         case 3://地址查找
             printf("请输入家庭住址：");
             scanf("%s", searAddress);
-            while (temp->next != NULL) {
+            while (temp->next != 0) {
                 if (strcmp(temp->address, searAddress) == 0) {
                     printf("%s %s %s", temp->name, temp->phoneNumber, temp->address);
+                    flag++;
                     break;
                 }
                 else{
@@ -78,26 +76,28 @@ void search(contact *head) {//查找功能
             }
             break;
         default:
-            printf("格式错误!\n");
+            printf("格式错误!");
     }
     if(flag>0){
-        printf("查找成功!\n");
+        printf("\n查找成功!\n");
     }
     else{
-        printf("查找失败!\n");
+        printf("\n查找失败!\n");
     }
 }
 //增加新的联系人
-void add(contact *head){
+int add(contact *head){
+    //已经通过测试
     contact *addman;//用于存储可能增加的联系人信息
-    contact *s=head;
-    addman=(contact*)malloc(sizeof(contact));//为可能新增的链表结点分配内存空间
+    contact *s;
+    s = head;
+    addman = (contact *)malloc(sizeof(contact)); //为可能新增的链表结点分配内存空间
     printf("请输入要添加的联系人姓名、电话号码以及住址，以空格分隔：");
     scanf("%s %s %s",addman->name,addman->phoneNumber,addman->address);
     while(s->next!=NULL){//遍历列表
         if((strcmp(addman->phoneNumber,s->phoneNumber)==0)&&(strcmp(addman->name,s->name)==0)){//判断是否存在，考虑有可能重名，故使用姓名号码双重确认
             printf("此联系人已存在！\n");
-            return;//存在时退出函数，不确定能不能这么写
+            return 0;//存在时退出函数，不确定能不能这么写
         }
         else{//不重复，继续读
             s=s->next;
@@ -105,28 +105,32 @@ void add(contact *head){
     }
     s->next=addman;//表尾插入新结点
     s=addman;
-    s->next=NULL;
+    s->next=0;
+    return 0;
 }
-void Delete(contact *head){//删除符合条件的结点
+int Delete(contact *head){
+    //测试通过
     contact *temp;//中间结点
     char delNum[15],delName[20];//存储输入的号码姓名数据
     printf("请输入要删除的联系人姓名：");
     scanf("%s",delName);
     printf("请输入要删除的联系人电话：");
     scanf("%s",delNum);
-    while(head->next!=NULL){//遍历链表
+    while(head->next!=0){//遍历链表
         if((strcmp(delNum,head->phoneNumber)==0)&&(strcmp(delName,head->name)==0)){//比较判定是否为要删除对象
             temp=head->next;//删除操作
             head->next=temp->next;
-            return;
+            printf("删除成功");
+            return 0;
         }
         else{
-            head=head->next;//继续便利
+            head=head->next;//继续遍历
         }
     }
 printf("删除失败!\n");
 }
 void change(contact *head){//更新联系人
+    //测试通过
     contact *temp=head;
     int mode=0;
     int flag=0;
@@ -142,7 +146,7 @@ void change(contact *head){//更新联系人
             char changeName[20];
             printf("请输入修改后的名字：");
             scanf("%s",changeName);
-            while(temp->next!=NULL){
+            while(temp->next!=0){
                 if ((strcmp(tempNum,temp->phoneNumber)==0)&&(strcmp(tempName,temp->name)==0)){
                     strcpy(temp->name,changeName);
                     flag=1;
@@ -157,7 +161,7 @@ void change(contact *head){//更新联系人
             char changeNum[20];
             printf("请输入修改后的号码：");
             scanf("%s", changeNum);
-            while (temp->next != NULL) {
+            while (temp->next != 0) {
                 if ((strcmp(tempNum, temp->phoneNumber) == 0) && (strcmp(tempName, temp->name) == 0)) {
                     strcpy(temp->phoneNumber, changeNum);
                     flag = 1;
@@ -171,7 +175,7 @@ void change(contact *head){//更新联系人
             char changeAddress[200];
             printf("请输入修改后的住址：");
             scanf("%s", changeAddress);
-            while(temp->next!=NULL){
+            while(temp->next!=0){
                 if ((strcmp(tempNum,temp->phoneNumber)==0)&&(strcmp(tempName,temp->name)==0)){
                     strcpy(temp->address,changeAddress);
                     flag=1;
@@ -190,22 +194,72 @@ void change(contact *head){//更新联系人
         printf("修改失败!\n");
     }
 }
-void update(contact *head){//更新存储文件
-    contact *s=head;
-    FILE *fp1,*fp2;
-    fp1=fopen("通讯录.txt","w");
-    while(s->next!=NULL){
-        fprintf(fp1,"%s %s %s\n",s->name,s->phoneNumber,s->address);
+void update(contact *head){//无法通过测试，请重新编写，现在暂时改成显示所有通讯录功能 
+    contact *s=head->next;
+    //FILE *fp1,*fp2;
+    //fp1=fopen("contacts.txt","w");
+    while(s!=0){
+        printf("%s %s %s\n", s->name, s->phoneNumber, s->address);
+        //fprintf(fp1, "%s %s %s\n", s->name, s->phoneNumber, s->address);
         s=s->next;
     }
-    printf("更新成功!\n");
+    //printf("更新成功!\n");
 }
 void SetNULL(contact *head){//链表和存储文件置空
-    head=NULL;
+    head=0;
     FILE *fp;
-    fp=fopen("通讯录.txt","w");
+    fp=fopen("contacts.txt","w");
     fprintf(fp,"\n");
 }
 int main() {
-
+    /*
+    注意：为了防止问题出现和便于进行调试
+    我已经将通讯录.txt更改为contacts.txt
+    其中的测试已经将中文换成英文
+    以上所有通过测试的函数均为在此环境下进行的测试
+    现在还无法存储更改，因为update函数没法用，原因未知
+    另外，SetNULL函数不知道有啥用，故未使用
+    */
+    printf("欢迎使用通讯录管理系统!");
+    contact *headp;
+    headp = Read();//读取文件
+    while (1)
+    {
+        printf("\n请选择接下来要进行的操作:\n1.查看全部通讯录\n2.增加联系人");
+        printf("\n3.删除联系人\n4.查找联系人\n5.更改联系人\n6.关闭程序\n请输入:");
+        //各个数字的功能如上
+        int choose;
+        scanf("%d",&choose);
+        //选择模块
+        switch(choose){
+            case 1:{
+                update(headp);
+                break;
+            }
+            case 2:{
+                add(headp);
+                break;
+            }
+            case 3:{
+                Delete(headp);
+                break;
+            }
+            case 4:{
+                search(headp);
+                break;
+            }
+            case 5:{
+                change(headp);
+                break;
+            }
+            case 6:{
+                exit(0);
+                break;
+            }
+            default:{
+                break;
+            }
+        }
+    }
+    return 0;
 }
